6fab9250ee7445e6c03a906f1547168415619e89
malekshafei <malek7shafei@gmail.com>
2024-12-03 22:42:00 -0500

    Add files via upload

--- Streamlit.py
+++ Streamlit.py
@@ -81,7 +81,7 @@ pos_list = ['CBs', 'WBs', 'CMs', 'AMs', 'Ws', 'STs']
 
 
 
-mode = st.selectbox("Select Mode", options=['Player Overview', 'Multi Player Dot Graph', 'Player Match by Match Performance', 'Team Style'])
+mode = st.selectbox("Select Mode", options=['Player Overview', 'Multi Player Dot Graph', 'Player Rankings',  'Team Style'])
 
 if mode == 'Player Overview':
 
@@ -1342,7 +1342,68 @@ if  mode == 'Multi Player Dot Graph':
     st.image(buf, use_column_width=True)
     radar = True
     position_group1 = 'NA'
-        
+
+if mode == 'Player Rankings':
+    position_group1 = 'CBs'
+    mode1 = ''
+
+    # URL of your Tableau dashboard (can be from Tableau Public or Server)
+    import streamlit.components.v1 as components
+
+    # Replace this with your Tableau Public embed link (copied from the "Embed Code" section)
+    tableau_embed_code = """
+    <div class='tableauPlaceholder' id='viz1733282530187' style='position: relative'>
+        <noscript>
+            <a href='#'>
+                <img alt='Dashboard 1' src='https://public.tableau.com/static/images/Lo/LouisvillePlayerData/Dashboard1/1_rss.png' style='border: none' />
+            </a>
+        </noscript>
+        <object class='tableauViz' style='display:none;'>
+            <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+            <param name='embed_code_version' value='3' />
+            <param name='site_root' value='' />
+            <param name='name' value='LouisvillePlayerData/Dashboard1' />
+            <param name='tabs' value='no' />
+            <param name='toolbar' value='yes' />
+            <param name='static_image' value='https://public.tableau.com/static/images/Lo/LouisvillePlayerData/Dashboard1/1.png' />
+            <param name='animate_transition' value='yes' />
+            <param name='display_static_image' value='yes' />
+            <param name='display_spinner' value='yes' />
+            <param name='display_overlay' value='yes' />
+            <param name='display_count' value='yes' />
+            <param name='language' value='en-US' />
+        </object>
+    </div>
+
+    <script type='text/javascript'>
+        var divElement = document.getElementById('viz1733282530187');
+        var vizElement = divElement.getElementsByTagName('object')[0];
+        if ( divElement.offsetWidth > 800 ) {
+            vizElement.style.width='1220px';
+            vizElement.style.minHeight='587px';
+            vizElement.style.maxHeight='887px';
+            vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+        } else if ( divElement.offsetWidth > 500 ) {
+            vizElement.style.width='1220px';
+            vizElement.style.minHeight='587px';
+            vizElement.style.maxHeight='887px';
+            vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+        } else {
+            vizElement.style.width='100%';
+            vizElement.style.height='727px';
+        }
+        var scriptElement = document.createElement('script');
+        scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+        vizElement.parentNode.insertBefore(scriptElement, vizElement);
+    </script>
+    """ 
+    tableau_embed_code = """
+    <div class='tableauPlaceholder' id='viz1733283562119' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Lo&#47;LouisvillePlayerData&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='LouisvillePlayerData&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Lo&#47;LouisvillePlayerData&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1733283562119');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.minWidth='504px';vizElement.style.maxWidth='774px';vizElement.style.width='100%';vizElement.style.minHeight='587px';vizElement.style.maxHeight='887px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.minWidth='504px';vizElement.style.maxWidth='774px';vizElement.style.width='100%';vizElement.style.minHeight='587px';vizElement.style.maxHeight='887px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+    """
+    # Embed the Tableau dashboard using st.components.v1.html
+    components.html(tableau_embed_code, height=600)
+
+
 if mode == 'Player Match by Match Performance':
     mode1 = 'NA'
     position_group1 = 'CBs'
